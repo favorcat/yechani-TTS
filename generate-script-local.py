124a23a7dac641413e2a7df1f74ee4a634c7dd31
favorcat <favorcat.me@gmail.com>
2022-03-21 00:39:47 +0900

    여러 파일 자동화

--- generate-script-local.py
+++ generate-script-local.py
@@ -1,4 +1,6 @@
-def transcribe_file(speech_file):
+import os
+
+def transcribe_file(dir, speech_file):
     """Transcribe the given audio file."""
     from google.cloud import speech
     import io
@@ -23,14 +25,26 @@ def transcribe_file(speech_file):
     response = client.recognize(config=config, audio=audio)
     
     # json 파일에 내용추가
-    with open("/Users/favorcat/Github/yechani-TTS/script-local.json", "a") as script:
+    with open(dir+"/script-local.json", "a") as script:
         for result in response.results:
             print('"'+speech_file+'": "'+u'{}'.format(result.alternatives[0].transcript)+'",')
             script.write('"'+speech_file+'": "'+u'{}'.format(result.alternatives[0].transcript)+'",\n')
-cnt = 530
-while(cnt <= 783):
-    # gs://~ 는 버킷 내부의 오디오 데이터 경로
-    transcribe_file("/Users/favorcat/Github/yechani-TTS/resource/output/"+str(cnt)+".wav")
-    cnt+=1
+            
+
+path_dir = '/Users/favorcat/Github/yechani-TTS/resource/' #경로 끝에 / 꼭 붙이기
+folder_list = os.listdir(path_dir) #경로 읽어 여러 폴더들 읽기
+folder_list.sort() #정렬
+
+for folder in folder_list:
+    if (folder != '.DS_Store' and folder != '210213 - 완료'):
+        folder_dir = os.path.join(path_dir,folder)
+        output_dir = os.path.join(folder_dir,'output/')
+        
+        file_list = os.listdir(output_dir)
+        file_list.sort()
+
+        for file in file_list:
+            # gs://~ 는 버킷 내부의 오디오 데이터 경로
+            transcribe_file(folder_dir, output_dir+file)
     
 # 오류 발생시 https://cloud.google.com/speech-to-text/docs/error-messages 참조
--- slient-divide.py
+++ slient-divide.py
@@ -4,6 +4,7 @@ https://www.thetopsites.net/article/54596077.shtml
 https://stackoverflow.com/questions/23730796/using-pydub-to-chop-up-a-long-audio-file
 '''
 
+import os
 # Import the AudioSegment class for processing audio and the 
 # split_on_silence function for separating out silent chunks.
 from pydub import AudioSegment
@@ -15,29 +16,38 @@ def match_target_amplitude(aChunk, target_dBFS):
     change_in_dBFS = target_dBFS - aChunk.dBFS
     return aChunk.apply_gain(change_in_dBFS)
 
-# Load your audio.
-song = AudioSegment.from_wav("/Users/favorcat/Github/yechani-TTS/resource/210213.wav")
+path_dir = '/Users/favorcat/Github/yechani-TTS/resource/' #경로 끝에 / 꼭 붙이기
+folder_list = os.listdir(path_dir) #경로 읽어 여러 폴더들 읽기
+folder_list.sort() #정렬
 
-# Split track where the silence is 2 seconds or more and get chunks using 
-# the imported function.
-chunks = split_on_silence (
-    # Use the loaded audio.
-    song, 
-    # Specify that a silent chunk must be at least 2 seconds or 2000 ms long.
-    min_silence_len = 400,
-    # Consider a chunk silent if it's quieter than -16 dBFS.
-    # (You may want to adjust this parameter.)
-    silence_thresh = -40
-)
+for folder in folder_list:
+    if (folder != '.DS_Store'):
+        folder_dir = os.path.join(path_dir,folder)
+        file_name = "{}.wav".format(folder)
+        file_dir = os.path.join(folder_dir,file_name)
 
-# Process each chunk with your parameters
-for i, chunk in enumerate(chunks):
-    print("chunk >> ", i)
-    # Create a silence chunk that's 0.5 seconds (or 500 ms) long for padding.
-    silence_chunk = AudioSegment.silent(duration=100)
-    # Add the padding chunk to beginning and end of the entire chunk.
-    audio_chunk = silence_chunk + chunk + silence_chunk
-    # Normalize the entire chunk.
-    normalized_chunk = match_target_amplitude(audio_chunk, -20.0)
-    # Export the audio chunk with new bitrate.
-    chunk.export("/Users/favorcat/Github/yechani-TTS/resource/output/{0}.wav".format(i), format="wav")
+        # Load your audio.
+        song = AudioSegment.from_wav(file_dir)
+        print(file_dir)
+
+        chunks = split_on_silence (
+            # Use the loaded audio.
+            song, 
+            # Specify that a silent chunk must be at least 2 seconds or 2000 ms long.
+            min_silence_len = 400,
+            # Consider a chunk silent if it's quieter than -16 dBFS.
+            # (You may want to adjust this parameter.)
+            silence_thresh = -40
+        )
+
+        # Process each chunk with your parameters
+        for i, chunk in enumerate(chunks):
+            print("chunk >> ", i)
+            # Create a silence chunk that's 0.5 seconds (or 500 ms) long for padding.
+            silence_chunk = AudioSegment.silent(duration=100)
+            # Add the padding chunk to beginning and end of the entire chunk.
+            audio_chunk = silence_chunk + chunk + silence_chunk
+            # Normalize the entire chunk.
+            normalized_chunk = match_target_amplitude(audio_chunk, -20.0)
+            # Export the audio chunk with new bitrate.
+            chunk.export("{0}/output/{1}.wav".format(folder_dir,i), format="wav")
