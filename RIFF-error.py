0aeffb77c7485e6603acb099e0c6db2ce0221c39
favorcat <favorcat.me@gmail.com>
2022-03-21 23:11:30 +0900

    edit: 여러 파일 자동화

--- RIFF-error.py
+++ RIFF-error.py
@@ -1,10 +1,16 @@
 import os
 import subprocess
 
-path_dir = '/Users/favorcat/Github/yechani-TTS/resource/output/' #경로 끝에 / 꼭 붙이기
-file_list =os.listdir(path_dir) #경로 읽어 파일명 리스트 만들기
-file_list.sort() #정렬
+path_dir = '/Users/favorcat/Github/yechani-TTS/resource/' #경로 끝에 / 꼭 붙이기
+folder_list = os.listdir(path_dir) #경로 읽어 여러 폴더들 읽기
+folder_list.sort() #정렬
 
-for file in file_list:
-    command = "ffmpeg -i {} -acodec pcm_s16le -ac 1 -ar 16000 {}".format(os.path.join(path_dir,file), os.path.join(path_dir,"RIFF-ID/",file))
-    subprocess.call(command, shell=True)
+for folder in folder_list:
+    if (folder != '.DS_Store'):
+        output_dir = os.path.join(path_dir,folder,'output/')
+        file_list = os.listdir(output_dir)
+        file_list.sort()
+        
+        for file in file_list:
+            command = "ffmpeg -i {} -acodec pcm_s16le -ac 1 -ar 16000 {}".format(os.path.join(path_dir,file), os.path.join(path_dir,"RIFF-ID/",file))
+            subprocess.call(command, shell=True)
--- get-wav-length.py
+++ get-wav-length.py
@@ -10,14 +10,20 @@ def get_duration(audio_path):
     duration = frames / float(rate)
     return duration
 
-path_dir = '/Users/favorcat/Github/yechani-TTS/resource/output/RIFF-ID/' #경로 끝에 / 꼭 붙이기
-file_list =os.listdir(path_dir) #경로 읽어 파일명 리스트 만들기
-file_list.sort() #정렬
+path_dir = '/Users/favorcat/Github/yechani-TTS/resource/' #경로 끝에 / 꼭 붙이기
+folder_list = os.listdir(path_dir) #경로 읽어 여러 폴더들 읽기
+folder_list.sort() #정렬
 
 res = 0
-for file in file_list:
-    path = path_dir+file
-    res += get_duration(path)
+for folder in folder_list:
+    if (folder != '.DS_Store'):
+        output_dir = os.path.join(path_dir,folder,'output/')
+        file_list = os.listdir(output_dir)
+        file_list.sort()
+
+        for file in file_list:
+            path = output_dir+file
+            res += get_duration(path)
 
 sec = res % 60
 min = int(res / 60 % 60)
